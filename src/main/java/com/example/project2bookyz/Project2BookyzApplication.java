040f3b052dd8194f398f42085e9da245d99ea854
Azrad Murad <nurulazradb@herbalife.com>
2021-11-20 07:27:18 +0800

    fresh copy of maven project

---
+++ src/main/java/com/example/project2bookyz/Project2BookyzApplication.java
@@ -0,0 +1,13 @@
+package com.example.project2bookyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2BookyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2BookyzApplication.class, args);
+	}
+
+}
